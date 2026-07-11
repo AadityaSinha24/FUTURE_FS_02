460109d4ca6a669eb80b5779ba404977aa2e5c12
AadityaSinha24 <adityasinha21024@gmail.com>
2026-07-11 23:37:58 +0530

    Setup pages and layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <BrowserRouter> */}
-    {/* </BrowserRouter> */}
-    
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        {/* Global toast notifications */}
+        <Toaster
+          position="top-right"
+          richColors
+          toastOptions={{ style: { borderRadius: "14px" } }}
+        />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
